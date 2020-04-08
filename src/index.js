1fa7a2b6ff278d96d58b82cd92f158ba23cca58d
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-08 17:27:22 +0400

    changes for testing WeekDay component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './containers/App';
+import WeekDay from "./components/WeekDAy";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { todos } from "./components/ToDos";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WeekDay id={todos[0].id} todo={todos[0].todo} day={todos[0].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[1].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[2].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[3].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[4].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[5].day} />
+    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[6].day} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
