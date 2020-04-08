0975a8ccca63038ef7de1e2e2a618e06889a33fb
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-08 22:28:46 +0400

    changed li element with todo property from DayList component

--- src/components/WeekDAy.js
+++ src/components/WeekDAy.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 const Day = (props) => {
+  const { day, todo } = props;
   return (
     <div className="bg-washed-red shadow-5 ma3 pa3 grow shadow-hover br3 dib w-20">
-      <h1 className="tc">{props.day}</h1>
-      <ul>
-        <li>{props.todo[0]}</li>
-      </ul>
+      <h1 className="tc">{day}</h1>
+      <ul>{todo}</ul>
     </div>
   );
 };
