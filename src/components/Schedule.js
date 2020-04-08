418e5aef953ba7a007c80df528066789a9b6f23a
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-08 22:19:39 +0400

    renamed and edited schedule array

---
+++ src/components/Schedule.js
@@ -0,0 +1,58 @@
+export const Schedule = [
+  {
+    todos: [
+      { todo: "Learn React", id: "k1" },
+      { todo: "Learn Vue", id: "k2" },
+    ],
+    id: "d1",
+    day: "Monday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k3" },
+      { todo: "Learn Vue", id: "k4" },
+    ],
+    id: "d2",
+    day: "Tuesday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k5" },
+      { todo: "Learn Vue", id: "k6" },
+    ],
+    id: "d3",
+    day: "Wednesday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k7" },
+      { todo: "Learn Vue", id: "k8" },
+    ],
+    id: "d4",
+    day: "Thursday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k9" },
+      { todo: "Learn Vue", id: "k10" },
+    ],
+    id: "d5",
+    day: "Friday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k11" },
+      { todo: "Learn Vue", id: "k12" },
+    ],
+    id: "d6",
+    day: "Saturday",
+  },
+  {
+    todos: [
+      { todo: "Learn React", id: "k13" },
+      { todo: "Learn Vue", id: "k14" },
+    ],
+    id: "d7",
+    day: "Sunday",
+  },
+];
