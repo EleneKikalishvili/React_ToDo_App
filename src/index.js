8474e6e386a8ba3b128ed4374c46377d8688b0ed
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-08 22:29:28 +0400

    rendering DayList component

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from './containers/App';
-import WeekDay from "./components/WeekDAy";
+import DayList from "./components/DayList";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
-import { todos } from "./components/ToDos";
 
 ReactDOM.render(
   <React.StrictMode>
-    <WeekDay id={todos[0].id} todo={todos[0].todo} day={todos[0].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[1].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[2].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[3].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[4].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[5].day} />
-    <WeekDay id={todos[1].id} todo={todos[1].todo} day={todos[6].day} />
+    <DayList />
   </React.StrictMode>,
   document.getElementById("root")
 );
