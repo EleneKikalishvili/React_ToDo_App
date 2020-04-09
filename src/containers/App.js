1be3886acc1a0e1ca61db72122cea9586bfb4df9
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-09 22:25:43 +0400

    added filtering functionality, rendering Schedule filtered by user input

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import DayList from "../components/DayList";
+import { Schedule } from "../components/Schedule";
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return <div className="App"></div>;
+    this.state = {
+      schedule: Schedule,
+      searchField: "",
+    };
+  }
+
+  render() {
+    const { schedule, searchField } = this.state;
+    const filteredSchedule = schedule.filter((day) =>
+      day.day.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="tc">
+        <input
+          className="ma5"
+          type="search"
+          placeholder="search"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
+        <DayList schedule={filteredSchedule} />
+      </div>
+    );
+  }
 }
-
 export default App;
