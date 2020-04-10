d879960702426d88952183dc6a1564e2bee3d892
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-04-10 15:37:47 +0400

    changed way of passing properties

--- src/components/WeekDAy.js
+++ src/components/WeekDAy.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const Day = (props) => {
-  const { day, todo } = props;
+const Day = ({ day, todo }) => {
   return (
     <div className="bg-washed-red shadow-5 ma3 pa3 grow shadow-hover br3 dib w-20">
       <h1 className="tc">{day}</h1>
